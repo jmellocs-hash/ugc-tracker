0778e0278b0b0894240222c88ed8c1b654ac6f0c
jmellocs-hash <jmellocs@gmail.com>
2026-02-15 21:53:54 -0500

    Add Home component for UGC Tracker

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>UGC Tracker is running 🚀</h1>
+      <p>Next step: we will add campaigns system.</p>
+    </main>
+  );
+}
