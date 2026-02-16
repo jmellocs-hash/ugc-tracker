06c9ce83b4d3139b60a7f7023a4dedb9e8d2ebdd
jmellocs-hash <jmellocs@gmail.com>
2026-02-15 22:00:36 -0500

    Update next step message in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import "./globals.css";
+
 export default function Home() {
   return (
     <main style={{ padding: 40 }}>
       <h1>UGC Tracker is running 🚀</h1>
-      <p>Next step: we will add campaigns system.</p>
+      <p>Next step: campaigns system.</p>
     </main>
   );
 }
